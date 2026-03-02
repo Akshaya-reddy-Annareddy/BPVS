d2becfc195b9078feee3ae287fd797732db142b7
Akshaya Reddy Anna Reddy <arudraar2023@gmail.com>
2026-03-02 15:51:46 +0000

    All errors are fixed

--- enrollment/core/core/settings.py
+++ enrollment/core/core/settings.py
@@ -56,21 +56,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "core.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = "core.wsgi.application"
 
 
@@ -119,7 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -129,7 +116,7 @@ USE_TZ = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # You can add template folders later
+        'DIRS': [BASE_DIR / 'templates'],  # IMPORTANT
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
